f6c82e178875a4e8d27a6e781e44c8882022d829
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-01-05 21:54:14 +0530

    Create MergeTwoSortedArrays.java

---
+++ Code/MergeTwoSortedArrays.java
@@ -0,0 +1,30 @@
+  /*
+    Problem Statement
+    -----------------
+    Given an array of length n, and an integer mid(mid<n). The subarray array[0...mid-1]
+    and array[mid...n-1] are sorted. Sort the complete array.
+    
+    Time Complexity  : O(n)
+    Space Complexity : O(1) 
+     */
+    public static void merge2SortedArrays(int[] array, int mid) {
+        int pos = 0;
+        int i = 0;
+        int j = mid;
+
+        while (i < mid && j < array.length) {
+            if (array[i] > array[j]) {
+                array[pos++] = array[j++];
+            } else {
+                array[pos++] = array[i++];
+            }
+        }
+
+        while (i < mid) {
+            array[pos++] = array[i++];
+        }
+
+        while (j < array.length) {
+            array[pos++] = array[j++];
+        }
+    }
