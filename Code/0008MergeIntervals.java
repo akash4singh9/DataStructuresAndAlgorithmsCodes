27d4d25454867caeb465c90cc471ab4579f7c6a1
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-01-05 21:50:50 +0530

    Update 0008MergeIntervals.java

--- Code/0008MergeIntervals.java
+++ Code/0008MergeIntervals.java
@@ -1,1 +1,29 @@
 
+    /*
+    Problem Statement
+    -----------------
+    Given a matrix of size m*2, each row is an interval on the number line.
+    Return a matrix of size n*2 (n<=m) where, again each row is an interval on 
+    the number line but this time compressed with the overlapping intervals
+    merged.
+    
+    Time Complexity  : O(m) m=rows
+    Space Complexity : O(1)
+     */
+    public int[][] mergeOverlappingIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<int[]>();
+        int[] current = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(intervals[i][1], current[1]);
+            } else {
+                merged.add(current);
+                current = intervals[i];
+            }
+        }
+
+        merged.add(current);
+        return merged.toArray(new int[merged.size()][]);
+    }
