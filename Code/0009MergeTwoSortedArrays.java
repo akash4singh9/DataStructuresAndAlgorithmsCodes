718b0583ec193d66f0f3d29755db980bf545de5d
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-01-05 23:48:46 +0530

    Update 0009MergeTwoSortedArrays.java

--- Code/0009MergeTwoSortedArrays.java
+++ Code/0009MergeTwoSortedArrays.java
@@ -5,14 +5,15 @@
     and array[mid...n-1] are sorted. Sort the complete array.
     
     Time Complexity  : O(n)
-    Space Complexity : O(1) 
+    Space Complexity : O(n) 
      */
     public static void merge2SortedArrays(int[] array, int mid) {
+        int[] auxiliary=new int[array.length]; 
         int pos = 0;
         int i = 0;
         int j = mid;
 
-        while (i < mid && j < array.length) {
+        while (i < mid && j < array.length) {`
             if (array[i] > array[j]) {
                 array[pos++] = array[j++];
             } else {
