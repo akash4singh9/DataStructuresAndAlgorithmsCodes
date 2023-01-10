77db0aa653ca061e5fced9ff818eebead500d408
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-01-10 11:31:53 +0530

    Update 0014MergeSort.java

--- Code/0014MergeSort.java
+++ Code/0014MergeSort.java
@@ -1,11 +1,21 @@
-   
 
 class Program {
 
   public static int[] auxArray;
   
-  
-  
+    /*
+    Problem Statement
+    -----------------
+    Given an array of length n, and four integers start1,end1,start2,end2 
+    end2=end1+1 and 0<=start1<=end1<=end2<=start2<n.
+    array is sorted between the indices start1 and end1 (both inclusive)
+    and between start2 and end2 (both inclusive).
+    Sort the array between start1 and end2 (both inclusive).
+    
+    Time Complexity  : O(k) , k=end2-start1+1
+    Space Complexity : O(n) 
+    */
+   
   public static void merge(int[] array,int start1, int end1,int start2,int end2 )
   {
     int pos=start1;
@@ -29,7 +39,7 @@ class Program {
   }
     
     /*
-    Problem Statement
+    (Helper)
     -----------------
     Given an array of size n, a start index and an end index sort the array.
     Sort the array between the start and end indices including both indices.
@@ -57,7 +67,7 @@ class Program {
     the array. Use merge sort algorithm.
     
     Time Complexity  : O(n.log(n)), log has base 2
-    Space Complexity : O(n
+    Space Complexity : O(n)
     */
   public static int[] mergeSort(int[] array) 
   {
