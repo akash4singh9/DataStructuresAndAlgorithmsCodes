005530a0fe5585f238bf319b66fc4b154857040a
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-01-10 08:31:31 +0530

    Update 0012InsertionSort.java

--- Code/0012InsertionSort.java
+++ Code/0012InsertionSort.java
@@ -2,9 +2,16 @@ import java.util.*;
 
 class Program {
 
-  //array 
-  //array is sorted till index-1
-
+    /*
+    (Helper)
+    -----------------
+    Given an array of size n, and an index i.
+    Array is non decreasing upto index i-1.
+    Make the array sorted(non decreasing) upto index i.
+    
+    Time Complexity  : O(n)
+    Space Complexity : O(1)
+    */
   public static void insert(int[] array,int index)
   {
     int swapIndex=index;
@@ -22,6 +29,16 @@ class Program {
     array[swapIndex]=elementAtIndex;
  }
   
+    /*
+    Problem Statement
+    -----------------
+    Given an array of size n, sort the array.
+    Sorted form of the array is the non decreasing permutation of 
+    the array. Use insertion sort algorithm.
+    
+    Time Complexity  : O(n^2)
+    Space Complexity : O(1)
+    */
   public static int[] insertionSort(int[] array) {
     for(int i=1;i<array.length;i++)
       insert(array,i);
