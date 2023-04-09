783ddfd8d7f30774ccc967cd25f911ac43c0061f
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-04-10 04:11:44 +0530

    Update ThreeNumberSort.java

--- Arrays/ThreeNumberSort.java
+++ Arrays/ThreeNumberSort.java
@@ -1,1 +1,68 @@
 
+    /*
+    Problem Statement
+    -----------------
+    Given an array input of size n and an array order of size 3.
+    The input consists of numbers only from order.
+    Rearrange the input in such a way that the elements must be in the order
+    as they are in order array.
+   
+    Time Complexity  : O(n)
+    Space Complexity : O(1)
+    */
+    public int[] threeNumberSort(int[] array, int[] order) {
+    // Write your code here.
+    int countOfOrder0=0;
+    int countOfOrder1=0;
+    int countOfOrder2=0;
+    
+    for(int i:array)
+      if(order[0]==i)
+        countOfOrder0++;
+      else if(order[1]==i)
+        countOfOrder1++;
+      else if(order[2]==i)
+        countOfOrder2++;
+
+    for(int i=0;i<countOfOrder0;i++)
+      array[i]=order[0];
+    for(int i=countOfOrder0;i<countOfOrder1+countOfOrder0;i++)
+      array[i]=order[1];
+    for(int i=countOfOrder1+countOfOrder0;i<countOfOrder1+countOfOrder0+countOfOrder2;i++)
+      array[i]=order[2];
+  
+    return array;
+    }
+    
+    /*
+    Another (Special case of above)
+    Problem Statement
+    -----------------
+    Given an array of size n,which only has 0s, 1s and 2s, sort it.
+    This is a case of the problem statement above when the order array
+    is [0,1,2].
+    Time Complexity  : O(n)
+    Space Complexity : O(1)
+    */
+    public static void sort012(byte[] array) {
+
+        int zeroes = 0;
+        int ones = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]==0)
+                zeroes++;
+            if(array[i]==1)
+                ones++;
+        }
+        
+        for(int i=0;i<array.length;i++)
+        {
+            if(zeroes>0)
+            {array[i]=0;zeroes--;}
+            else if(ones>0)
+            {  array[i]=1;ones--;}
+            else
+                array[i]=2;
+        }
+    }
