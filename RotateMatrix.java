3433d94a6cb87a9a6658275818490f0f37c6e7f7
Akash Singh <108982833+akash4singh9@users.noreply.github.com>
2023-04-12 13:58:33 +0530

    Create RotateMatrix.java

---
+++ RotateMatrix.java
@@ -0,0 +1,50 @@
+    /*
+    (Helper)
+    --------
+    Given a square matrix, transpose it
+    
+    Time Complexity  : O(m*n) m=rows n=columns
+    Space Complexity : O(1)
+     */
+    public static void transposeSquareMatrix(int[][] matrix) {
+        int temp = -1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    /*
+    (Helper)
+    --------
+    Given a square matrix, flip matrix horizontally
+    
+    Time Complexity  : O(m*n) m=rows n=columns
+    Space Complexity : O(1)
+     */
+    public static void flipMatrixHorizontally(int[][] matrix) {
+        int temp = -1;
+        for (int i = 0; i <(matrix.length / 2); i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - i - 1][j];
+                matrix[matrix.length - i - 1][j] = temp;
+            }
+        }
+    }
+
+    /*
+    Problem Statement
+    -----------------
+    Given a square matrix, rotate matrix anticlockwise by 90 degree
+    
+    Time Complexity  : O(m*n) m=row size n=column size
+    Space Complexity : O(1)
+     */
+    public static void rotateMatrixAntiClockwise90Degree(int[][] matrix) {
+        transposeSquareMatrix(matrix);
+        flipMatrixHorizontally(matrix);
+    }
